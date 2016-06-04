97548189e569ff3aa5161a8b5424569b3316c031
Stuart Mitchelhill <stuart.mitchelhill@gmail.com>
2016-06-04 20:26:24 +1000

    node setup

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Utasko' });
+  res.render('index', 
+  	{ 
+  		title: 'Utasko' 
+  	});
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', 
+  	{ 
+  		title: 'Utasko | Login' 
+  	});
+});
+
+/* GET Sign Up page. */
+router.get('/sign_up', function(req, res, next) {
+  res.render('sign_up', 
+  	{ 
+  		title: 'Utasko | Sign Up' 
+  	});
+});
+
+/* GET Project page. */
+router.get('/project', function(req, res, next) {
+  res.render('project', 
+  	{ 
+  		title: 'Utasko | Project Name' 
+  	});
 });
 
 module.exports = router;
